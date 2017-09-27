2dfb0b3db8d4589db0dbfce8114df613ae6dd260
adhikarivijay05 <adhikari.vijay05@example.comgit config --global user.email adhikari.vijay05@example.com>
2017-09-27 16:07:48 +0530

    image picker
    image picker

--- imagePicker/ViewController.swift
+++ imagePicker/ViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var userimage: UIImageView!
+    
+    
+    let imagePicker = UIImagePickerController()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
 
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
+        
+            userimage.image = image
+            imagePicker.dismiss(animated: true, completion: nil)
+        }else{
+        print("error ")
+            
+        }
+    
+    }
+    @IBAction func cameraClicked(_ sender: UIBarButtonItem) {
+        
+        imagePicker.sourceType = .savedPhotosAlbum
+        imagePicker.allowsEditing = false
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+
+    
+    
 }
 
